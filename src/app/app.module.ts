b45ca6baac815c9b2120430900e22bb21a7de20d
Vireth <mdk2-kevin@hotmail.fr>
2017-08-04 16:40:05 +0200

    Add Angular Material 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule}  from  '@angular/platform-browser/animations';
+import { MdButtonModule, MdIconModule } from '@angular/material';
 
+import 'hammerjs';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Angular Animation & Material
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
